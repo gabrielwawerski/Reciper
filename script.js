1d1b092dbbe5a7c7f46baebf137fbd8964fe7cfc
4masm <gabriel.wawerski@gmail.com>
2020-05-10 23:48:55 +0100

    commit

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-let requestUrl = 'https://gabrielwawerski.github.io/Reciper/recipes.json';
+const REQUEST_URL = 'https://gabrielwawerski.github.io/Reciper/recipes.json';
 let request = new XMLHttpRequest();
 
-request.open('GET', requestUrl);
+request.open('GET', REQUEST_URL);
 request.responseType = 'json';
 request.send();
 
-request.onload = function() {
+request.onload = function () {
     let recipes = request.response;
-    console.log(recipes);
+    console.log(recipes.productList);
 };
 
 const PRODUCT_LIST_TABLE = document.getElementById("product-list-table");
 const RECIPE_DESCRIPTION = document.getElementById("recipe-description");
+const RECIPE_CONTAINER = document.getElementsByClassName("recipe-container");
 
+// const RECIPE_TITLE = RECIPE_CONTAINER.appendChild(document.createElement('recipe'));
+
+// RECIPE_TITLE.innerText = recipes[1].name;
+
+
+// RECIPE_CONTAINER.append(RECIPE_TITLE);
