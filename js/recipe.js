c76bdd2c01409fe4bc96b35852f7c8792c73fc04
4masm <gabriel.wawerski@gmail.com>
2020-05-11 20:20:46 +0100

    commit

--- js/recipe.js
+++ js/recipe.js
@@ -2,7 +2,6 @@ const RECIPE_NAME = document.getElementById('recipe-name');
 const RECIPE_IMG = document.getElementById('recipe-img');
 const INGREDIENTS_LIST = document.getElementById('ingredients-table');
 const RECIPE_LIST = document.getElementById('recipe-description-data');
-console.log(RECIPE_LIST);
 
 RECIPE_NAME.innerText = localStorage.getItem("name");
 RECIPE_IMG.innerText = localStorage.getItem("img");
@@ -10,7 +9,6 @@ RECIPE_IMG.innerText = localStorage.getItem("img");
 
 let ingredients = localStorage.getItem("productList").split(",");
 let recipeDescription = localStorage.getItem("instructions").split("-");
-console.log(recipeDescription);
 
 for (let i = 0; i < ingredients.length; i++) {
     const LIST_ITEM = document.createElement('li');
