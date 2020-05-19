26c9781de54d59cda348fbbb2d25baf5a198ca08
4masm <gabriel.wawerski@gmail.com>
2020-05-19 17:40:51 +0100

    CSS tweaks, visual changes

--- js/script.js
+++ js/script.js
@@ -19,9 +19,11 @@ class Recipe {
         this.checkbox = checkbox;
     }
 }
+
 Element.prototype.remove = function() {
     this.parentElement.removeChild(this);
 };
+
 NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
     for(let i = this.length - 1; i >= 0; i--) {
         if(this[i] && this[i].parentElement) {
